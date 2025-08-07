f4a44bd4e0c9c65f5fda99f7561556b926387f48
stargazerwh <m18852647428@163.com>
2025-08-07 22:34:36 +0600

    remove the never existing parameter from `GoogleGenerativeAIEmbeddings()` function for creating gemini embeddings

--- src/raglight/embeddings/gemini_embeddings.py
+++ src/raglight/embeddings/gemini_embeddings.py
@@ -41,6 +41,5 @@ class GeminiEmbeddingsModel(EmbeddingsModel):
         """
         return GoogleGenerativeAIEmbeddings(
             model=self.model_name,
-            google_api_key=Settings.GEMINI_API_KEY,
-            google_api_base=self.api_base,
+            google_api_key=Settings.GEMINI_API_KEY
         )
