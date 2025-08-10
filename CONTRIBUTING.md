ec05cd40417236e11316aa538b7758b194dbcd0c
stargazerwh <m18852647428@163.com>
2025-08-10 11:50:00 +0200

    fix: add CONTRIBUTING.md and create builder tests

---
+++ CONTRIBUTING.md
@@ -0,0 +1,22 @@
+## Contributing to RAGLight
+
+
+### Installing project locally
+
+
+Start with forking the repository, then clone pull it to your local workspace with 
+```bash
+git clone git@github.com:Bessouat40/RAGLight.git
+```
+
+Install environment and dependencies using 
+```
+uv sync
+```
+
+
+### Running Tests
+
+Simply run `python3 -m unittest` to run all tests 
+
+
--- tests/tests_rag/test_builder.py
+++ tests/tests_rag/test_builder.py
@@ -1,11 +1,49 @@
 import unittest
-from ...src.raglight.rag.builder import Builder
+import logging
+from unittest.mock import patch, MagicMock
+from raglight.config.settings import Settings
+from raglight.rag.builder import Builder
 
+logging.getLogger().setLevel(logging.WARNING)
 
 class TestRAGBuilder(unittest.TestCase):
-    def setUp(self):
-        self.builder = Builder()
-        self.assertEqual(True, True, "Null test")
+    @patch("raglight.rag.builder.ChromaVS")
+    def test_builder_rag(self, mock_chroma):
+        mock_chroma_instance = MagicMock()
+        mock_chroma.return_value = mock_chroma_instance
+
+        rag = (
+            Builder()
+            .with_embeddings(Settings.OLLAMA, model_name="gpt-oss:20b")
+            .with_vector_store(
+                Settings.CHROMA, persist_directory="/", collection_name="test"
+            )
+            .with_llm(Settings.OLLAMA, model_name="gpt-oss:20b")
+            .build_rag()
+        )
+
+        mock_chroma.assert_called_once()
+        self.assertIsNotNone(rag)
+
+class TestRATBuilder(unittest.TestCase):
+    @patch("raglight.rag.builder.ChromaVS")
+    def test_builder_rag(self, mock_chroma):
+        mock_chroma_instance = MagicMock()
+        mock_chroma.return_value = mock_chroma_instance
+
+        rat = (
+            Builder()
+            .with_embeddings(Settings.OLLAMA, model_name="gpt-oss:20b")
+            .with_vector_store(
+                Settings.CHROMA, persist_directory="/", collection_name="test"
+            )
+            .with_llm(Settings.OLLAMA, model_name="gpt-oss:20b")
+            .with_reasoning_llm(Settings.OLLAMA, model_name="deepseek:r1:20b")
+            .build_rat()
+        )
+
+        mock_chroma.assert_called_once()
+        self.assertIsNotNone(rat)
 
 
 if __name__ == "__main__":
