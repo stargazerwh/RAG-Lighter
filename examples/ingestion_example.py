2f85011311f8a4e0b2b11d744f8b010446565dc3
stargazerwh <m18852647428@163.com>
2025-08-14 22:40:20 +0200

    refactor code to simplify code ingestion

--- examples/ingestion_example.py
+++ examples/ingestion_example.py
@@ -11,11 +11,9 @@ model_embeddings = Settings.DEFAULT_EMBEDDINGS_MODEL
 collection_name = Settings.DEFAULT_COLLECTION_NAME
 data_path = os.environ.get('DATA_PATH')
 
-file_extension = Settings.DEFAULT_EXTENSIONS # All visible files
-
 vector_store = Builder() \
 .with_embeddings(Settings.HUGGINGFACE, model_name=model_embeddings) \
 .with_vector_store(Settings.CHROMA, persist_directory=persist_directory, collection_name=collection_name) \
 .build_vector_store()
 
-vector_store.ingest(file_extension=file_extension, data_path=data_path)
+vector_store.ingest(data_path=data_path)
--- src/raglight/config/vector_store_config.py
+++ src/raglight/config/vector_store_config.py
@@ -10,7 +10,6 @@ class VectorStoreConfig:
     api_base: str = field(default=Settings.DEFAULT_OLLAMA_CLIENT)
     provider: str = field(default=Settings.HUGGINGFACE)
     database: str = field(default=Settings.CHROMA)
-    file_extension: str = field(default=Settings.DEFAULT_EXTENSIONS)
     collection_name: str = field(default=Settings.DEFAULT_COLLECTION_NAME)
     ignore_folders: list = field(
         default_factory=lambda: list(Settings.DEFAULT_IGNORE_FOLDERS)
